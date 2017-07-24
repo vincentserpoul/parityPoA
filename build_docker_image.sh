dbf92b610d2087b894b80adaab1d97041dce0bb3
Vincent Serpoul <vincent@serpoul.com>
2017-07-24 16:10:42 +0800

    update to 1.6.10

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker pull parity/parity:v1.6.9
+# docker pull parity/parity:v1.6.10
 
-docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.6.9 ./docker
+docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.6.10 ./docker
