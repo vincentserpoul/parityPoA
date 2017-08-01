bf6497161e8dd9bba40adcd02b917f4718ac07f4
Vincent Serpoul <vincent@serpoul.com>
2017-08-01 12:10:41 +0800

    update to 1.7.0

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker pull parity/parity:v1.6.10
+# docker pull parity/parity:v1.7.0
 
-docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.6.10 ./docker
+docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.7.0 ./docker
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,7 @@
 # Run parity
 /parity/parity daemon 1234 --config /etc/parityPoA/configPoAInit.toml &
 
-sleep 5s
+sleep 10s
 
 # Import the authority account from the passphrase
 curl --data "{\"jsonrpc\":\"2.0\",\"method\":\"parity_newAccountFromPhrase\",\"params\":[\"$(cat /run/secrets/authority.$AUTHORITY_COMPANY.passphrase)\", \"$(cat /run/secrets/authority.$AUTHORITY_COMPANY.password)\"],\"id\":0}" -H "Content-Type: application/json" -X POST 127.0.0.1:8545
@@ -19,5 +19,4 @@ sed -i 's/AUTHORITY_COMPANY/'$AUTHORITY_COMPANY'/' /etc/parityPoA/configPoANode.
     --ui-port=$UI_PORT \
     --jsonrpc-port=$JSONRPC_PORT \
     --port=$NETWORK_PORT \
-    --dapps-port=$DAPPS_PORT \
     --gasprice 0
