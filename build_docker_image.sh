da155c93b3262ddcb76db685dedfeab8c339f40b
Vincent Serpoul <vincent@serpoul.com>
2017-11-17 13:35:55 +0800

    working parity extension

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # docker pull parity/parity:v1.8.1
 
-docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.8.1 ./docker
+docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.8.3 ./docker
