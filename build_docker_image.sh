1bdd2bcfdb310f511b9d88fe9a2bb65dbb785fab
Vincent Serpoul <vincent@serpoul.com>
2018-04-17 14:25:35 +0800

    update 1.10.1

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker pull parity/parity:v1.9.4
+# docker pull parity/parity:v1.10.1
 
-docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.9.4 ./docker
+docker build -f ./docker/Dockerfile -t vincentserpoul/paritypoa:v1.10.1 ./docker
