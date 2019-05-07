3ea0bd7ed032540e107999725fd1c49cf99b052b
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-05-07 10:51:26 +0200

    Updating to 2.5.0

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker pull parity/parity:stable
+# docker pull parity/parity:v2.5.0
 
-docker build -f ./docker-image/Dockerfile -t vincentserpoul/paritypoa:stable ./docker-image
+docker build -f ./docker-image/Dockerfile -t vincentserpoul/paritypoa:v2.5.0 ./docker-image
